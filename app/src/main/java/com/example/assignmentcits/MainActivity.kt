65548183a89c8a18e48bf017cf3547e870f423b7
Satnam Singh(JustCode) <satnam.2003singh2003@gmail.com>
2023-09-28 21:26:52 +0530

    Third Commit

--- app/src/main/java/com/example/assignmentcits/MainActivity.kt
+++ app/src/main/java/com/example/assignmentcits/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         replaceFragment(FragmentHome())
     }
     private fun replaceFragment(fragment : Fragment){
+
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout,fragment)
